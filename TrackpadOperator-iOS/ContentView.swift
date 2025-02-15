3d100a782a14d4225be92b78d0e6ea3afe4c92ab
khoanisdev <doanvankhoan124@gmail.com>
2025-02-15 07:19:44 +0700

    Initial Commit

---
+++ TrackpadOperator-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackpadOperator-iOS
+//
+//  Created by Đoàn Văn Khoan on 15/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackpadOperator-iOS/TrackpadOperator_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackpadOperator_iOSApp.swift
+//  TrackpadOperator-iOS
+//
+//  Created by Đoàn Văn Khoan on 15/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackpadOperator_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
